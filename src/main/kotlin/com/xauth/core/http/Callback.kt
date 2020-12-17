cde42af2047f8aff3a6b695f3508b59a88ea007b
abc <you@example.com>
2020-12-17 17:30:04 +0800

    fix

--- src/main/kotlin/com/xauth/core/http/Callback.kt
+++ src/main/kotlin/com/xauth/core/http/Callback.kt
@@ -1,8 +1,8 @@
 package com.xauth.core.http
 
-//import com.xauth.core.graphql.GraphQLResponse.ErrorInfo
+import com.xauth.core.graphql.GraphQLResponse.ErrorInfo
 
 interface Callback<T> {
     fun onSuccess(result: T)
-//    fun onFailure(error: ErrorInfo?)
+    fun onFailure(error: ErrorInfo?)
 }
